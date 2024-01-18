9e33ca34f897b2e9a966d016ef01530e10fec22b
chayden1009 <cosmiccaleb51@gmail.com>
2024-01-18 10:13:12 -0600

    Create mood, song, customuser models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import CustomUser, Song, Mood
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+  add_form = CustomUserCreationForm
+  form = CustomUserChangeForm
+  model = CustomUser
+  list_display = ("email", "is_staff", "is_active",)
+  list_filter = ("email", "is_staff", "is_active",)
+
+  fieldsets = (
+    (None, {"fields": ("email", "password", "favorites")}),
+    ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+  )
+  add_fieldsets = (
+    (None, {
+      "classes": ("wide",),
+        "fields": (
+          "email", "password1", "password2", "is_staff",
+            "is_active", "groups", "user_permissions"
+        )}
+    ),
+  )
+
+  search_fields = ("email",)
+  ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Song)
+admin.site.register(Mood)
