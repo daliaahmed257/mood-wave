e4ec67a5c07ed5e5236a48062d5c99f69eedd92b
HeatherRouke <heather214@gmail.com>
2024-01-18 15:58:54 -0500

    recreate db

--- main_app/models.py
+++ main_app/models.py
@@ -7,26 +7,18 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-
-
 class Mood(models.Model):
   title = models.CharField()
-  description = models.TextField()
-
-class Song(models.Model):
-    title = models.CharField()
-    artist = models.CharField()
-    Hyperlink = models.CharField()
-    url = models.CharField(max_length=200)
-    mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
-
-
-class Mood(models.Model):
-  title = models.CharField()
-  playlist = models.ForeignKey(Song, on_delete=models.CASCADE)
   
-
-
+  
+class Song(models.Model):
+  title = models.CharField()
+  artist = models.CharField()
+  Hyperlink = models.CharField()
+  url = models.CharField(max_length=200)
+  mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+ 
+  
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
   email = models.EmailField(_("email address"), unique=True)
@@ -46,10 +38,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Journal(models.Model):
   content = models.TextField(default="")
   date = models.DateTimeField(timezone.now)
-  user = models.ForeignKey(CustomUser)
-class Journal(models.Model):
-  content = models.TextField(default="")
-  date = models.DateTimeField(timezone.now)
-  user = models.ForeignKey(CustomUser)
+  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
 
 
--- moodwave/settings.py
+++ moodwave/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 LOGIN_REDIRECT_URL = '/moods/'
 
