812cee6ed79e08c8aee13ab6372a5cb30898d66f
chayden1009 <cosmiccaleb51@gmail.com>
2024-01-19 09:21:32 -0600

    Updated models.py, reverse from create to detail

--- main_app/models.py
+++ main_app/models.py
@@ -3,23 +3,29 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from .managers import CustomUserManager
 from django.utils.translation import gettext_lazy as _
-
+from django.urls import reverse
 
 # Create your models here.
 
-MOOD_CHOICES = {
-  "HAPPY": "Happy",
-  "CALM": "Calm",
-  "SAD": "Sad",
-  "BORED": "Bored",
-  "ANXIOUS": "Anxious",
-  "ANGRY": "Angry"
-}
+MOOD_CHOICES = [
+        ("HAPPY", "Happy"),
+        ("CALM", "Calm"),
+        ("SAD", "Sad"),
+        ("BORED", "Bored"),
+        ("ANXIOUS", "Anxious"),
+        ("ANGRY", "Angry"),
+    ]
 
 class Mood(models.Model):
   title = models.CharField(max_length=7, choices=MOOD_CHOICES)
+  content = models.TextField(default="")
+  date = models.DateTimeField(timezone.now, default=timezone.now)
+
   def __str__(self):
         return self.title
+  def get_absolute_url(self):
+      return reverse("detail", kwargs={"mood_id": self.pk})
+  
   
   
 class Song(models.Model):
@@ -47,10 +53,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
   def __str__(self):
     return self.email
 
-class Journal(models.Model):
-  content = models.TextField(default="")
-  date = models.DateTimeField(timezone.now)
-  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  
+  
+  
 
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Mood, Song, CustomUser, Journal
+from .models import Mood, Song, CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.views.generic import CreateView, UpdateView, DeleteView
 
@@ -34,13 +34,7 @@ def playlists(request):
 
 class CreateMood(CreateView):
   model = Mood
-  fields = ["title"]
-  success_url = "/moods/"
-
-  def form_valid(self, form):
-    form.instance.user = self.request.user
-
-    return super().form_valid(form)
+  fields = ["title", "content"]
   
 
 
